32c76b1b3847e4ed670292f0587fca5aeb5f69df
Vaibhav Chauhan <chauhanvaibhavsingh0@gmail.com>
2025-09-24 17:15:28 +0530

    Hooks on CRM with Playwright

--- tests/crm.spec.js
+++ tests/crm.spec.js
@@ -5,49 +5,38 @@ import { homepage } from '../POM pages/homepage';
 import { leadscreationpage } from '../POM pages/leadCreationPage';
 import { leadspage } from '../POM pages/leadspage';
 import { logoutpage } from '../POM pages/logoutpage';
+const dataset=JSON.parse(JSON.stringify(require("../Testdata/testDataCRM.json")));
 
-test('First PW test', async ({browser})=>
-{
-    const context= await browser.newContext();
-    const page = await context.newPage();
-    await page.pause();
+let page;
 
-    await page.goto('http://49.249.28.218:8888/');
+test.beforeEach(async ({browser})=>{
+    page=await browser.newPage();
+    await page.goto(dataset.url);
 
     const lp = new loginpage(page);
-    const username ='admin';
-    const userpassword='admin';
-    await lp.logincredentials(username,userpassword);
-    // await page.goto('http://49.249.28.218:8888/');
-    // await page.locator("//input[@name='user_name']").fill("admin");
-    // await page.locator("//input[@name='user_password']").fill("admin");
-    // await page.locator('#submitButton').click();
+    await lp.logincredentials(dataset.username,dataset.userpassword);
+
+
+});
+
+test('CRM Framework', async ({browser})=>
+{
 
     const hp =new homepage(page);
     await hp.homepageclick();
-    // await page.locator('a',{hasText:'Leads'}).click();
 
     const lcp = new leadscreationpage(page);
     await lcp.leadsbtnclick();
-    // await page.locator("//img[@title='Create Lead...']").click();
 
     const ldp = new leadspage(page);
-    const lastname="Samsung";
-    const title="Mobile";
-    await ldp.leadscredentials(lastname,title);
-    // await page.locator("//input[@name='lastname']").fill("Samsung");
-    // await page.locator("//input[@name='company']").fill("Mobile");
-    // await page.locator("//input[@type='submit' and @class='crmButton small save' ]").click();
+    await ldp.leadscredentials(dataset.lastname,dataset.title);
+
+
+});
+
+test.afterEach(async ()=>{
 
     const lop = new logoutpage(page);
     await lop.signout();
-    // await page.locator("//img[@src='themes/softed/images/user.PNG']").hover();
-    // await page.locator('a',{hasText:'Sign Out'}).click();
-
-    await page.close();
-    await context.close();
-    await browser.close();
-
-
 
 });
